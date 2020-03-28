a105e06deb33801aa6897eb984a9acc60543918f
goyal-aman <goyal.amanrocks@gmail.com>
2020-03-28 23:42:52 +0530

    added single enemy login

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pygame
-from utilities import Character
+import random
+from utilities import Character, Enemy
 import colors
 pygame.init()
 
@@ -13,14 +14,23 @@ pygame.display.set_icon(icon)
 
 # ArcadeShip
 initialX, initialY = 250, 380
-ArcadeShipImg = pygame.image.load('./media/ShipImg.png')
-ArcadeShip = Character(initialY, initialY, 50, 50, ArcadeShipImg)
-ArcadeShip.Img = pygame.transform.scale(ArcadeShipImg, (50, 50))
+ArcadeShipImg = pygame.image.load('./media/shipimg64.png')
+ArcadeShip = Character(initialX, initialY, 64, 64, ArcadeShipImg)
 
 
 # ArcadeShip Actions
 def ShowArcadeShip():
     win.blit(ArcadeShip.Img, (ArcadeShip.posX, ArcadeShip.posY))
+
+
+# Enemies
+EnemyImg = pygame.image.load('./media/enemy32.png')
+enemy = Enemy(32, 32, EnemyImg, win_width, win_height/2)
+
+
+# Enemy Actions
+def ShowEnemy():
+    win.blit(enemy.Img, (enemy.posX, enemy.posY))
 
 
 run = True
@@ -31,19 +41,25 @@ while run:
             pygame.quit()
     win.fill(colors.Black)
 
-    key_press = pygame.key.get_pressed()
-    if key_press[pygame.K_RIGHT]:
-        if ArcadeShip.posX + 50 < win_width:
-            ArcadeShip.move_right()
-    if key_press[pygame.K_LEFT]:
-        if ArcadeShip.posX > 0:
-            ArcadeShip.move_left()
-    if key_press[pygame.K_UP]:
-        if ArcadeShip.posY > 0:
-            ArcadeShip.move_up()
-    if key_press[pygame.K_DOWN]:
-        if ArcadeShip.posY + 50 < win_height:
-            ArcadeShip.move_down()
-
+    # refreshing movement thousand time, for better movement
+    # set loop to 1, and remove move_right, move_left..etc parameters
+    # for lower spec device
+    for _ in range(1000):
+        ''' arcade ship movement '''
+        key_press = pygame.key.get_pressed()
+        if key_press[pygame.K_RIGHT]:
+            if ArcadeShip.posX + ArcadeShip.width < win_width:
+                ArcadeShip.move_right(0.003)
+        if key_press[pygame.K_LEFT]:
+            if ArcadeShip.posX > 0:
+                ArcadeShip.move_left(0.003)
+        if key_press[pygame.K_UP]:
+            if ArcadeShip.posY > 0:
+                ArcadeShip.move_up(0.003)
+        if key_press[pygame.K_DOWN]:
+            if ArcadeShip.posY + ArcadeShip.height < win_height:
+                ArcadeShip.move_down(0.003)
+    enemy.move()
     ShowArcadeShip()
+    ShowEnemy()
     pygame.display.update()
--- utilities.py
+++ utilities.py
@@ -1,10 +1,19 @@
+import random
+import math
+
+
 class Character:
-    def __init__(self, posX, posY, width, height, ship_img):
+    '''
+    posX, posY -> (x, y) cords
+    widht, height -> (width, height) of character
+    '''
+
+    def __init__(self, posX, posY, width, height, character_img):
         self.posX = posX
         self.posY = posY
         self.width = width
         self.height = height
-        self.Img = ship_img
+        self.Img = character_img
 
     def move_right(self, dx=0.1):
         self.posX += dx
@@ -21,3 +30,30 @@ class Character:
     def move_character(self, dx, dy):
         self.posX += dx
         self.posY += dy
+
+
+class Enemy:
+    '''
+    posX, posY -> (x, y) cords
+    widht, height -> (width, height) of character
+    character_img -> image of character (python.pygame image object)
+    Hrange, Vrange -> horizontal and vertical range where enemy can roam
+    '''
+
+    def __init__(self, width, height, character_img, Hrange, Vrange):
+        self.posX = random.randint(0, Hrange)
+        self.posY = random.randint(0, Vrange)
+        self.width = width
+        self.height = height
+        self.Img = character_img
+        self.Hrange = Hrange
+        self.Vrange = Vrange
+        self.dx = random.random()*2
+        self.dy = None
+
+    def move(self):
+        if self.posX+self.width > self.Hrange:
+            self.dx *= -1
+        if self.posX < 0:
+            self.dx *= -1
+        self.posX += self.dx
