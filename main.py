73a8b95d9186a3e9a2e6cf8264e8bb7c1649d8f5
goyal-aman <goyal.amanrocks@gmail.com>
2020-03-28 20:22:59 +0530

    added arcade ship

--- main.py
+++ main.py
@@ -1,14 +1,49 @@
 import pygame as pygame
+from ship import Ship
+import colors
 pygame.init()
 
-win_width, win_height = 500,500
+win_width, win_height = 500, 500
 win = pygame.display.set_mode((win_width, win_height))
 
-run=True
+# title and icon
+pygame.display.set_caption("Space Wars")
+icon = pygame.image.load('./media/game icon/alien.png')
+pygame.display.set_icon(icon)
+
+# ArcadeShip
+initialX, initialY = 250, 380
+ArcadeShipImg = pygame.image.load('./media/ShipImg.png')
+ArcadeShip = Ship(initialY, initialY, 50, 50, ArcadeShipImg)
+ArcadeShip.Img = pygame.transform.scale(ArcadeShipImg, (50, 50))
+
+
+# ArcadeShip Actions
+def ShowArcadeShip():
+    win.blit(ArcadeShip.Img, (ArcadeShip.posX, ArcadeShip.posY))
+
+
+run = True
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
             pygame.quit()
-    
+    win.fill(colors.Black)
+
+    key_press = pygame.key.get_pressed()
+    if key_press[pygame.K_RIGHT]:
+        if ArcadeShip.posX + 50 < win_width:
+            ArcadeShip.move_right()
+    if key_press[pygame.K_LEFT]:
+        if ArcadeShip.posX > 0:
+            ArcadeShip.move_left()
+    if key_press[pygame.K_UP]:
+        if ArcadeShip.posY > 0:
+            ArcadeShip.move_up()
+    if key_press[pygame.K_DOWN]:
+        if ArcadeShip.posY + 50 < win_height:
+            ArcadeShip.move_down()
+
+    ShowArcadeShip()
     pygame.display.update()
---
+++ ship.py
@@ -0,0 +1,23 @@
+class Ship:
+    def __init__(self, posX, posY, width, height, ship_img):
+        self.posX = posX
+        self.posY = posY
+        self.width = width
+        self.height = height
+        self.Img = ship_img
+
+    def move_right(self, dx=0.1):
+        self.posX += dx
+
+    def move_left(self, dx=0.1):
+        self.posX -= dx
+
+    def move_up(self, dy=0.1):
+        self.posY -= dy
+
+    def move_down(self, dy=0.1):
+        self.posY += dy
+
+    def move_ship(self, dx, dy):
+        self.posX += dx
+        self.posY += dy
